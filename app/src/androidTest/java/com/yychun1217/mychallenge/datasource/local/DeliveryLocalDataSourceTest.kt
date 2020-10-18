79766233b098904431ec6e49370b55bb05451477
yychun <yuyauchun@gmail.com>
2020-10-18 17:22:33 +0800

    add unit test for datasource

---
+++ app/src/androidTest/java/com/yychun1217/mychallenge/datasource/local/DeliveryLocalDataSourceTest.kt
@@ -0,0 +1,50 @@
+package com.yychun1217.mychallenge.datasource.local
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.yychun1217.mychallenge.BaseTestCase
+import com.yychun1217.mychallenge.db.DeliveryDao
+import com.yychun1217.mychallenge.dummy.Dummy
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class DeliveryLocalDataSourceTest : BaseTestCase() {
+
+    private lateinit var deliverLocalDataSource: DeliveryLocalDataSource
+
+    @Mock
+    private lateinit var deliveryDao: DeliveryDao
+
+    @Before
+    fun createDeliveryLocalDataSource() {
+        deliverLocalDataSource = DeliveryLocalDataSource(deliveryDao)
+    }
+
+    @Test
+    fun testGetDeliveryByIDReturnDelivery() = runBlocking {
+        Mockito.`when`(deliveryDao.get(Dummy.DELIVER_ID)).thenReturn(Dummy.DELIVERY_DB)
+        val delivery = deliverLocalDataSource.getDelivery(Dummy.DELIVER_ID)
+        assert(delivery?.id == Dummy.DELIVER_ID)
+    }
+
+    @Test
+    fun testGetDeliveryByWrongIDReturnNull() = runBlocking {
+        Mockito.`when`(deliveryDao.get(Dummy.DELIVER_ID)).thenReturn(Dummy.DELIVERY_DB)
+        val delivery = deliverLocalDataSource.getDelivery(Dummy.DELIVER_WRONG_ID)
+        assert(delivery == null)
+    }
+
+    @Test
+    fun testUpdate() = runBlocking {
+        Mockito.`when`(deliveryDao.update(Dummy.DELIVERY_DB_UPDATED)).thenReturn(Unit)
+        val isSuccess = deliverLocalDataSource.update(Dummy.DELIVERY_DB_UPDATED)
+        assert(isSuccess)
+    }
+}
---
+++ app/src/androidTest/java/com/yychun1217/mychallenge/datasource/local/DeliveryLocalRepositoryTest.kt
@@ -0,0 +1,55 @@
+package com.yychun1217.mychallenge.datasource.local
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.yychun1217.mychallenge.BaseTestCase
+import com.yychun1217.mychallenge.dummy.Dummy
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class DeliveryLocalRepositoryTest : BaseTestCase() {
+    private lateinit var deliverLocalRepository: DeliveryLocalRepository
+    @Mock
+    private lateinit var iDeliveryLocalDataSource: IDeliveryLocalDataSource
+
+    @Before
+    fun createDeliveryLocalRepository() {
+        deliverLocalRepository = DeliveryLocalRepository(iDeliveryLocalDataSource)
+    }
+
+    @Test
+    fun testGetDeliveryByIDReturnDelivery() = runBlocking {
+        Mockito.`when`(iDeliveryLocalDataSource.getDelivery(Dummy.DELIVER_ID)).thenReturn(Dummy.DELIVERY_DB)
+        val delivery = deliverLocalRepository.getDelivery(Dummy.DELIVER_ID)
+        assert(delivery?.id == Dummy.DELIVER_ID)
+    }
+
+    @Test
+    fun testGetDeliveryByWrongIDReturnNull() = runBlocking {
+        Mockito.`when`(iDeliveryLocalDataSource.getDelivery(Dummy.DELIVER_ID)).thenReturn(Dummy.DELIVERY_DB)
+        val delivery = deliverLocalRepository.getDelivery(Dummy.DELIVER_WRONG_ID)
+        assert(delivery == null)
+    }
+
+    @Test
+    fun testUpdateSuccessful() = runBlocking {
+        val returnIsSuccess = true
+        Mockito.`when`(iDeliveryLocalDataSource.update(Dummy.DELIVERY_DB_UPDATED)).thenReturn(returnIsSuccess)
+        val isSuccess = deliverLocalRepository.update(Dummy.DELIVERY_DB_UPDATED)
+        assert(isSuccess == returnIsSuccess)
+    }
+
+    @Test
+    fun testUpdateUnsuccessful() = runBlocking {
+        val returnIsSuccess = false
+        Mockito.`when`(iDeliveryLocalDataSource.update(Dummy.DELIVERY_DB_UPDATED)).thenReturn(returnIsSuccess)
+        val isSuccess = deliverLocalRepository.update(Dummy.DELIVERY_DB_UPDATED)
+        assert(isSuccess == returnIsSuccess)
+    }
+}
